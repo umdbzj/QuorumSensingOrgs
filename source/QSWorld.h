f7b3593c3df14f001ff7a015d3c298dbecb3cb54
Charles Ofria <charles.ofria@gmail.com>
2017-05-26 22:21:45 -0400

    Started setting up Updates to QSWorld.

--- source/QSWorld.h
+++ source/QSWorld.h
@@ -7,11 +7,30 @@ class QSWorld {
 private:
   emp::Random random;
   emp::evo::GridWorld<QSOrg> world;
+  size_t ai_radius;
 
 public:
-  QSWorld() {
-    world.ConfigPop(20,20);
+  QSWorld(size_t width=100, size_t height=100, size_t _air=10) : ai_radius(_air) {
+    world.ConfigPop(width,height);
     world.Insert( QSOrg(0.0, 0.0, 0.0) );
     world.Print(PrintOrg);
+  }
+
+  double GetAveAI(size_t id) {
+    return 0.0;
+  }
+
+  void Update(size_t new_resources=10) {
+    // Give each organism extra resources.
+    for (size_t i = 0; i < world.GetSize(); i++) {
+      if (world.IsOccupied(i) == false) continue;
+      world[i].AddResource(new_resources);
+    }
+
+    // Check to see if each organism produces public goods.
+    for (size_t i = 0; i < world.GetSize(); i++) {
+      if (world.IsOccupied(i) == false) continue;
+      
+    }
   }
 };
