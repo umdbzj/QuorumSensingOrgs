42942860bdcb919ea52c74e605ad92c7915acaf3
Charles Ofria <charles.ofria@gmail.com>
2017-05-26 23:26:10 -0400

    Build native QSWorld and run it for 100 updates; no muts yet.

--- source/native/quorum_sensing.cc
+++ source/native/quorum_sensing.cc
@@ -7,5 +7,10 @@
 int main()
 {
   std::cout << "Hello World!" << std::endl;
-  QSWorld world;
+  QSWorld world(20, 20, 5);
+
+  for (size_t ud = 0; ud < 100; ud++) {
+    world.Update();
+  }
+  world.Print();
 }
